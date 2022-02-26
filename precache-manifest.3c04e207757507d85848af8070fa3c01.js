12dd44e9dfcf5782a691928552bc9403d35001e6
Oscar Zhang <oscarzhang@Oscars-MacBook-Air.local>
2022-02-25 21:45:29 -0500

    version 1.0

---
+++ precache-manifest.3c04e207757507d85848af8070fa3c01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3664de4ce93273f53a8d",
+    "url": "/personalwebsite/static/js/runtime~main.3664de4c.js"
+  },
+  {
+    "revision": "3d58a6d874ea9dda8c35",
+    "url": "/personalwebsite/static/js/main.3d58a6d8.chunk.js"
+  },
+  {
+    "revision": "8ab068f70fd3b27e37e3",
+    "url": "/personalwebsite/static/js/2.8ab068f7.chunk.js"
+  },
+  {
+    "revision": "3d58a6d874ea9dda8c35",
+    "url": "/personalwebsite/static/css/main.11b5c8ec.chunk.css"
+  },
+  {
+    "revision": "8237564ed3ea731c9dcbebb5cf865def",
+    "url": "/personalwebsite/index.html"
+  }
+];
